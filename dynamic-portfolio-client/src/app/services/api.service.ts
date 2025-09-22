979b1c181fc6d1a752c9e99e5b04dddb61059b54
Ashish4883 <aashishmishra0016@gmail.com>
2025-09-22 14:23:31 +0530

    feat: Implement API service and environment configuration for HTTP requests

---
+++ dynamic-portfolio-client/src/app/services/api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  // private baseUrl = 'http://127.0.0.1:3333'; // Adonis backend URL
+  private baseUrl = environment.base;
+
+  constructor(private http: HttpClient) {}
+
+  getHello(): Observable<any> {
+    return this.http.get(`${this.baseUrl}hello`);
+  }
+
+  post(url: any, data: any, options?: any) {
+    return this.http.post(environment.base + url, data, options);
+  }
+
+  put(url: any, data: any, options?: any) {
+    return this.http.put(environment.base + url, data, options);
+  }
+
+  delete(url: any, options?: any) {
+    return this.http.delete(environment.base + url, options);
+  }
+
+  get(url: any, options?: any) {
+    return this.http.get(environment.base + url, options);
+  }
+}
---
+++ dynamic-portfolio-client/src/environments/environment.ts
@@ -0,0 +1,7 @@
+const base = 'https://dynamic-portfolio-server.onrender.com/';
+
+export const environment = {
+  production: false,
+  base: base,
+  socket: base, // if you want socket URL later
+};
